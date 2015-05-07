928dca8019299d39030f17fe46757d565d42329b
Sam Beran <sberan@gmail.com>
2015-05-07 11:43:38 -0500

    show all transactions
    this will need to be paginated :)

--- app/models/transaction.js
+++ app/models/transaction.js
@@ -1,8 +1,13 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
 export default DS.Model.extend({
   amount: DS.attr('number'),
   date: DS.attr('date'),
-  description: DS.attr('description'),
-  balance: DS.attr('number')
+  description: DS.attr('string'),
+  balance: DS.attr('number'),
+  formattedDate: Ember.computed('date', function() {
+    var date = this.get('date');
+    return "" + (date.getMonth() + 1) + "-" + date.getDate() + "-" + date.getFullYear()
+  })
 });
--- server/mocks/transactions.js
+++ server/mocks/transactions.js
@@ -17,7 +17,7 @@ module.exports = function(app) {
   
   transactionsRouter.get('/', function(req, res) {
     res.send({
-      'transactions': allTransactions.slice(0, 10)
+      'transactions': allTransactions
     });
   });
 
