a756674da97aa070be22938be64f7d753974da46
Sam Beran <sberan@gmail.com>
2015-05-08 16:54:38 -0500

    use an array proxy

--- app/app.js
+++ app/app.js
@@ -13,18 +13,51 @@ App = Ember.Application.extend({
   Resolver: Resolver
 });
 
-DS.RecordArray.reopen({
+var PaginatedArray = Ember.ArrayProxy.extend(Ember.MutableArray, {
+  init: function() {
+    this.set('content', Ember.A());
+    this.set('backingContent', this.get('delegate.content'));
+    this.set('store', this.get('delegate.store'));
+    this.set('type', this.get('delegate.type'));
+    this.set('backingStore', this.get('delegate.content'));
+    this.get('content').pushObjects(this.get('backingStore'));
+  },
+  
   nextPage: function() {
     var self = this;
-    //TODO: grab meta off object if possible
-    var meta = self.store.metadataFor(self.type);
+    var store = this.get('store');
+    var type = this.get('type');
+    //TODO: grab meta off object if possible (findQuery)
+    var meta = store.metadataFor(type);
     if (!meta || !meta.cursor) {
       throw new Error("no cursor metadata found")
     }
     
     //TODO: add original query params for query results
     //TODO: handle loading, error states
-    self.store.findQuery(self.type, {cursor: meta.cursor.next});
+    store.findQuery(type, {cursor: meta.cursor.next}).then(function(results) {
+      // the only way I can trigger an update is to create an entirely new array here.
+      var newContent = Ember.A();
+      newContent.pushObjects(self.get('content'));
+      newContent.pushObjects(results.get('content'));
+      self.set('content', newContent)
+    });
+  }
+})
+
+DS.PromiseArray.reopen({
+  paginate: function(opts) {
+    return this.then(function(contents) {
+      return PaginatedArray.create({delegate: contents})
+    })
+  }
+  //todo: add promisy implementations of other methods?
+});
+
+
+DS.RecordArray.reopen({
+  paginate: function() {
+    return PaginatedArray.create({delegate: this})
   }
 });
 
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model:function() {
-    return this.store.find("transaction");
+    return this.store.find("transaction").paginate();
   },
   
   actions: {
