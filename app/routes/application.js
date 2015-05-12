60cb826cbb38c0ecc6d152052477a6823f72ae7b
Sam Beran <sberan@gmail.com>
2015-05-12 09:00:32 -0500

    create test route to test back button

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,19 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model:function() {
-    return this.store.find("transaction").paginate({
-      pageSize: 100,
-      behavior: 'replace'
-    });
-  },
-  
-  actions: {
-    next: function() {
-      this.currentModel.nextPage()
-    },
-    prev: function() {
-      this.currentModel.prevPage()
-    }
+  beforeModel: function() {
+    this.transitionTo('transaction-list')
   }
 })
